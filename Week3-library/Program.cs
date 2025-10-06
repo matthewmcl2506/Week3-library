750b5172496db1aa01cb51d4c9e7fa6ddc8f885f
matthewmcl2506 <McLearnon-M1@ulster.ac.uk>
2025-10-06 15:20:49 +0100

    FINAL

--- Week3-library/Program.cs
+++ Week3-library/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Test");
+﻿namespace Library
+{
+    class Book
+    {
+        string Title;
+        string Author;
+        string ISBN;
+
+        void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            // Create a new instance (object) of the Book class
+            // Note how the object name differs from the class name
+            Book book = new Book();
+
+            // This information is for one book in our library
+            book.Title = "C# for beginners";
+            book.Author = "BillGates";
+            book.ISBN = "12345678";
+
+            // This is another book object.
+            // Note that the object name is 'book1' and not 'book'
+            Book book1 = new Book();
+            book1.Title = "C# Methods and classes";
+            book1.Author = "Microsoft";
+            book1.ISBN = "55667778";
+
+            // Output book information to the console
+            book.DisplayInfo();
+            book1.DisplayInfo();
+        }
+    }
+}
+   
