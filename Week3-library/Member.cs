9a11263b5f77f9fa9c31a5361abdb6e241cefdd8
matthewmcl2506 <McLearnon-M1@ulster.ac.uk>
2025-10-06 15:39:59 +0100

    Update

--- Week3-library/Member.cs
+++ Week3-library/Member.cs
@@ -11,6 +11,7 @@ namespace Library
         private string title; // private variable
         private string author;// private variable
         private string isbn; // private variable
+        
 
         // Title property allows access
         // to the title private variable
@@ -45,6 +46,26 @@ namespace Library
             Console.WriteLine($"Book Author: {Author}");
             Console.WriteLine($"Book ISBN: {ISBN}");
             Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+
+            // This information is for one book in our library
+            book.Title = "C# for beginners";
+            book.Author = "BillGates";
+            book.ISBN = "12345678";
+
+            // This is another book object.
+            // Note that the object name is 'book1' and not 'book'
+            Book book1 = new Book();
+            book1.Title = "C# Methods and classes";
+            book1.Author = "Microsoft";
+            book1.ISBN = "55667778";
+
+            // Output book information to the console
+            book.DisplayInfo();
+            book1.DisplayInfo();
         }
     }
 }
